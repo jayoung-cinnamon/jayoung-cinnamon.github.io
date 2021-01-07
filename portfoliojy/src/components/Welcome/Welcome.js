24987f83bc2fbf2792260215c2517d726809e233
jayoung-cinnamon <coffeejayoung@gmail.com>
2021-01-08 00:06:31 +0900

    styled components Test

--- portfoliojy/src/components/Welcome/Welcome.js
+++ portfoliojy/src/components/Welcome/Welcome.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import './Welcome.css';
+import  styled from 'styled-components';
+
+const WelcomeSection = styled.section`
+  color: blue;
+  border: 1px solid red;
+  margin-top: 100px;
+  height: 100vh;
+`;
+
+const WelcomeInner = styled.div`
+ display: flex;
+  border: 1px solid blue;
+  margin: 0 auto;
+  width: 80%;
+  height: 100vh;
+`;
+
+const WelcomeWrapper = styled.div`
+  border: 1px solid green;
+  margin-top: 0;
+  font-size: 50px;
+  color: black;
+`;
+
+const Title = styled.div`
+  font-size: 120px;
+  border: 1px solid red;
+`;
+
+const SubTitle = styled.div`
+  font-size: 30px;
+  border: 1px solid green;
+`;
 
 function Welcome(){
   return(
-    <section className="section__welcome">
-      <div className="section__welcome inner">
-        <div className="section__welcome wrapper">
-          <div className="section__welcome-title">Hi</div>
-          <div className="section__welcome-subtitle">Hi</div>
-        </div>
-      </div>
-    </section>
+    <WelcomeSection>
+      <WelcomeInner>
+        <WelcomeWrapper>
+          <Title>HI</Title>
+          <SubTitle>jayoung</SubTitle>
+        </WelcomeWrapper>
+      </WelcomeInner>
+    </WelcomeSection>
   );
 }
 
